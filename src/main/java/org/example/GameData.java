91a4f0ea987889a63b048357a1fd9046b8a61f33
ashton <ashton.ennis13@gmail.com>
2026-04-17 10:52:50 -0500

    add pre-existing files

---
+++ src/main/java/org/example/GameData.java
@@ -0,0 +1,184 @@
+package org.example;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileReader;
+import java.util.*;
+
+public class GameData {
+
+    private static final long seed = System.nanoTime();
+    private static final Random generator = new Random(seed);
+
+    static ArrayList<Integer> deck = new ArrayList<>();
+    static Stack<Integer> discardPile = new Stack<>();
+
+    public static int displayCard(int cardID) {
+        int id = 100;
+        try{
+            CSVReader reader = new CSVReaderBuilder(new FileReader("C:\\Users\\ashto\\Documents\\CS491\\uno3\\src\\main\\resources\\cardData.csv"))
+                    .withSkipLines(cardID + 1).build();
+
+            String[] nextline;
+            nextline = reader.readNext();
+            if(nextline != null) {
+                id = Integer.parseInt(nextline[1]);
+                System.out.print("id: " + nextline[1]); // index is column number
+                System.out.print(" | value: " + nextline[2]); // index is column number
+                System.out.print(" | color: " + nextline[3]); // index is column number
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("\nFinished.");
+        return(id);
+    };
+
+    public static void playCardTest() { // test
+        System.out.println("playCardTest");
+
+        System.out.println("\nIndex of card to play:");
+        Player.displayHand(Player.hand);
+        System.out.print("Current Card: ");
+        displayCard(GameData.discardPile.peek());
+
+        Scanner playCardScanner = new Scanner(System.in);
+        Integer cardSelection = playCardScanner.nextInt();
+        playCardScanner.nextLine();
+
+        int cardIndex = Player.hand.get(cardSelection) + 1;
+
+        if (GameData.testPlayable(cardIndex)) {
+            // display chosen card, discard pile, and new hand
+            // (remember, hand array is just card id, so only card id is selected & added)
+            System.out.println(Player.hand.get(cardIndex - 1));
+            GameData.discardPile.add(Player.hand.get(cardIndex - 1)); // add to discard pile
+            Player.hand.remove(cardIndex - 1); // remove from player hand
+
+            Player.displayHand(Player.hand); // display new hand
+            playCardTest();
+        }
+        else {
+            System.out.println("Please select a valid card.");
+            playCardTest();
+        }
+        return;
+    }
+
+    // test card playability
+    public static boolean testPlayable(int cardID) {
+
+        String playColor = "", playValue= "", discardValue="", discardColor = "";
+        int playColorInt, playValueInt, discardValueInt, discardColorInt;
+
+        try {
+            CSVReader playableReader = new CSVReaderBuilder(new FileReader("C:\\Users\\ashto\\Documents\\CS491\\uno3\\src\\main\\resources\\cardData.csv"))
+                    .withSkipLines(cardID + 1).build();
+
+            CSVReader discardReader = new CSVReaderBuilder(new FileReader("C:\\Users\\ashto\\Documents\\CS491\\uno3\\src\\main\\resources\\cardData.csv"))
+                    .withSkipLines(GameData.discardPile.peek() + 1).build();
+
+            System.out.println(GameData.discardPile.peek());
+
+            String[] nextlinePlay;
+            nextlinePlay = playableReader.readNext();
+            if (nextlinePlay != null) {
+                playColor = (nextlinePlay[3]);
+                playValue = (nextlinePlay[2]); // get value
+            }
+
+            String[] nextlineDiscard;
+            nextlineDiscard = discardReader.readNext();
+            if (nextlineDiscard != null) {
+                discardColor = (nextlineDiscard[3]); // get color
+                discardValue = (nextlineDiscard[2]); // get value
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        System.out.print("Before setting int value:\n"
+                + "playColorInt: " +   " playColor: " + playColor
+                + "\nplayValueInt: " +   " playValue: " + playValue
+                + "\ndiscardColorInt: " +   " discardColor: " + discardColor
+                + "\ndiscardValueInt: " +   " discardValue: " + discardValue + "\n\n");
+
+        playColorInt = Integer.parseInt(playColor);
+        playValueInt = Integer.parseInt(playValue);
+        discardColorInt = Integer.parseInt(discardColor);
+        discardValueInt = Integer.parseInt(discardValue);
+
+        System.out.print("After setting int value:\n"
+                     + "playColorInt: " + playColorInt + " playColor: " + playColor
+                     + "\nplayValueInt: " + playValueInt + " playValue: " + playValue
+                     + "\ndiscardColorInt: " + discardColorInt + " discardColor: " + discardColor
+                     + "\ndiscardValueInt: " + discardValueInt + " discardValue: " + discardValue + "\n");
+
+        if (playColorInt == discardColorInt || playValueInt == discardValueInt) {
+            System.out.println("True");
+            return(true);
+        }
+        else {
+            System.out.println("False");
+            return(false);
+        }
+    }
+
+    public static Stack<Integer> discardInit() {
+        int randomIndex = generator.nextInt(deck.size()); // generate a random index from deck array
+        int card = deck.remove(randomIndex); // remove a number from arraylist and set it as card
+        discardPile.add(card); // add that index value to discard
+        return discardPile;
+    }
+
+    public static ArrayList<Integer> deckInit (int deckSize) {
+
+        for(int i = 0; i <= deckSize; i++) {
+            deck.add(i);
+        }
+
+        System.out.println(deck + "\ndeckInit finished.");
+        return deck;
+    }
+
+/*
+    public static void gameMenu() {
+        System.out.println("Welcome to UNO!\nChoose an option:\n1. Play\n2.Settings (WIP)\n3.Close game.\n");
+        Scanner scanner = new Scanner(System.in);
+
+        Integer select = scanner.nextInt();
+        switch (select) {
+            default:
+                System.out.println("Please select a valid option.");
+                gameMenu();
+            case 1:
+                startGame();
+                break;
+            case 2:
+                Settings();
+                break;
+            case 3:
+                System.out.println("Goodbye!");
+                System.exit(0);
+                break;
+        }
+    }
+*/
+
+/*
+    public static void startGame() {
+        System.out.println("Started.");
+    }
+
+    public static void Settings() {
+        System.out.println("Settings.");
+    }
+*/
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Player {
+
+    static ArrayList<Integer> hand = new ArrayList<>();
+    private static final long seed = System.nanoTime();
+    private static final Random generator = new Random(seed);
+
+    public static void displayHand(ArrayList<Integer> hand) {
+        int cardID = 0;
+
+        for(int i = 0; i < hand.size(); i++) {
+            System.out.print(i + 1 + ": ");
+            cardID = hand.get(i);
+
+            try {
+                CSVReader reader = new CSVReaderBuilder(new FileReader("C:\\Users\\ashto\\Documents\\CS491\\uno3\\src\\main\\resources\\cardData.csv"))
+                        .withSkipLines(cardID + 1).build();
+
+                String[] nextline;
+                nextline = reader.readNext();
+                if (nextline != null) {
+                    System.out.print(Arrays.toString(nextline) + " ");
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("");
+        }
+    };
+
+    public static ArrayList<Integer> drawStartHand(int handSize, ArrayList<Integer> deck) {
+        for (int i = 0; i < handSize && !deck.isEmpty(); i++) {
+            int randomIndex = generator.nextInt(deck.size()); // generate a random index from deck array
+            int card = deck.remove(randomIndex); // remove a number from arraylist and set it as card
+            hand.add(card); // add that index value to hand
+        }
+
+        System.out.println(hand + "\ndrawStartHand finished.");
+        return new ArrayList<>(hand);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
