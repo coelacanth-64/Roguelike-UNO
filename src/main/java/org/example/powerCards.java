d131daf0d1ebeea88fde92d2c543268ea096791b
ashton <ashton.ennis13@gmail.com>
2026-04-27 16:05:30 -0500

    Delete PowerCards.java

--- src/main/java/org/example/powerCards.java
+++ src/main/java/org/example/powerCards.java
@@ -1,10 +1,8 @@
 package org.example;
 
-import java.util.Scanner;
-
 /*
     The purpose of this class is to implement the functionality of the power cards.
 */
 
-public class PowerCards {
+public class powerCards {
 }
